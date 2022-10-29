23db9544561c8a53b99252b023a5f89973571537
biancapower <bianca@biancapower.com>
2022-10-29 21:36:49 +1100

    Create reusable component and pass image src via props

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,13 +1,62 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
   return (
     <View>
       <Text>Image Screen</Text>
-      {/* <Image source={require("../../assets/beach.jpg")} />
-      <Image source={require("../../assets/forest.jpg")} />
-      <Image source={require("../../assets/mountain.jpg")} /> */}
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <ImageDetail
+          title="Forest"
+          imageSource={require("../../assets/forest.jpg")}
+          score={9}
+        />
+        <ImageDetail
+          title="Beach"
+          imageSource={require("../../assets/beach.jpg")}
+          score={7}
+        />
+        <ImageDetail
+          title="Mountain"
+          imageSource={require("../../assets/mountain.jpg")}
+          score={4}
+        />
+        <ImageDetail
+          title="Forest"
+          imageSource={require("../../assets/forest.jpg")}
+          score={9}
+        />
+        <ImageDetail
+          title="Beach"
+          imageSource={require("../../assets/beach.jpg")}
+          score={7}
+        />
+        <ImageDetail
+          title="Mountain"
+          imageSource={require("../../assets/mountain.jpg")}
+          score={4}
+        />
+        <ImageDetail
+          title="Forest"
+          imageSource={require("../../assets/forest.jpg")}
+          score={9}
+        />
+        <ImageDetail
+          title="Beach"
+          imageSource={require("../../assets/beach.jpg")}
+          score={7}
+        />
+        <ImageDetail
+          title="Mountain"
+          imageSource={require("../../assets/mountain.jpg")}
+          score={4}
+        />
+      </ScrollView>
     </View>
   );
 }
