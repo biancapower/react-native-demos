6808b519648e618ad8878416e5b2bd41679bc590
biancapower <bianca@biancapower.com>
2022-10-29 18:57:29 +1100

    Add multiple elements to the screen

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
   const greeting = 'Welcome to the components screen!';
-  return <Text style={styles.textStyle}>{greeting}</Text>;
+  const name = <Text>Peter Fry</Text>;
+
+  return (
+    // View is a container that can hold other elements
+    <View>
+      <Text style={styles.textStyle}>{greeting}</Text>
+      <Text>This is a second element</Text>
+      {name}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
