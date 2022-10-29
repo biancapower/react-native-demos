d75af9e9e16ad72ad52c8a971ef4c66703edc984
biancapower <bianca@biancapower.com>
2022-10-29 18:38:23 +1100

    Add new component to stack navigator

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
 
+// Decides what is shown on the screen
+// The key is the name of the route
+// The value is the component that is shown
+// The HomeScreen component is shown when the user navigates to the route named "Home"
+// The ComponentsScreen component is shown when the user navigates to the route named "Components"
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components: ComponentsScreen,
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Components",
     defaultNavigationOptions: {
       title: "App",
     },
