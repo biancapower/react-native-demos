ea314f718e38579886d7caeec5233574863aeb7c
biancapower <bianca@biancapower.com>
2022-10-30 11:58:46 +1100

    Validate state changes

--- src/screens/ColourPickerScreen.js
+++ src/screens/ColourPickerScreen.js
@@ -9,22 +9,35 @@ const ColourPickerScreen = () => {
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const setColour = (colour, change) => {
+    switch (colour) {
+      case 'red':
+        return (red + change > 255 || red + change < 0) ? null : setRed(red + change);
+      case 'green':
+        return (green + change > 255 || green + change < 0) ? null : setGreen(green + change);
+      case 'blue':
+        return (blue + change > 255 || blue + change < 0) ? null : setBlue(blue + change);
+      default:
+        return;
+    }
+  };
+
   return (
     <View>
       <ColourPickerDetail
         colour="Red"
-        onIncrease={() => setRed(red + COLOUR_INCREMENT)}
-        onDecrease={() => setRed(red - COLOUR_INCREMENT)}
-      />
+        onIncrease={() => setColour('red', COLOUR_INCREMENT)}
+        onDecrease={() => setColour('red', -1 * COLOUR_INCREMENT)}
+        />
       <ColourPickerDetail
         colour="Green"
-        onIncrease={() => setGreen(green + COLOUR_INCREMENT)}
-        onDecrease={() => setGreen(green - COLOUR_INCREMENT)}
-      />
+        onIncrease={() => setColour('green', COLOUR_INCREMENT)}
+        onDecrease={() => setColour('green', -1 * COLOUR_INCREMENT)}
+        />
       <ColourPickerDetail
         colour="Blue"
-        onIncrease={() => setBlue(blue + COLOUR_INCREMENT)}
-        onDecrease={() => setBlue(blue - COLOUR_INCREMENT)}
+        onIncrease={() => setColour('blue', COLOUR_INCREMENT)}
+        onDecrease={() => setColour('blue', -1 * COLOUR_INCREMENT)}
       />
       <View style={{
         height: 100,
