5c09967a05bc0a8a5a7d81deab467fe3dc4dd95a
biancapower <bianca@biancapower.com>
2022-10-30 13:19:37 +1100

    Update reducer to use conventions 'type' and 'payload'

--- src/screens/ColourPickerScreen.js
+++ src/screens/ColourPickerScreen.js
@@ -7,23 +7,23 @@ const COLOUR_INCREMENT = 25;
 // action -> how to change our state object
 const reducer = (state, action) => {
   // state === { red: number, green: number, blue: number }
-  // action === { colourToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+  // action === { type: 'red' || 'green' || 'blue', payload: 15 || -15 }
 
-  // action.colourToChange === 'red', 'green', or 'blue'
-  // action.amount === 15 or -15
-  switch (action.colourToChange) {
-    case "red":
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+  // action.type === 'red', 'green', or 'blue'
+  // action.payload === 15 or -15
+  switch (action.type) {
+    case "change_red":
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
-        : { ...state, red: state.red + action.amount };
-    case "green":
-      return state.green + action.amount > 255 || state.green + action.amount < 0
+        : { ...state, red: state.red + action.payload };
+    case "change_green":
+      return state.green + action.payload > 255 || state.green + action.payload < 0
         ? state
-        : { ...state, green: state.green + action.amount };
-    case "blue":
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : { ...state, green: state.green + action.payload };
+    case "change_blue":
+      return state.blue + action.payload > 255 || state.blue + action.payload < 0
         ? state
-        : { ...state, blue: state.blue + action.amount };
+        : { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -64,18 +64,18 @@ const ColourPickerScreen = () => {
     <View>
       <ColourPickerDetail
         colour="Red"
-        onIncrease={() => dispatch({ colourToChange: "red", amount: COLOUR_INCREMENT })}
-        onDecrease={() => dispatch({ colourToChange: "red", amount: -1 * COLOUR_INCREMENT })}
+        onIncrease={() => dispatch({ type: "change_red", payload: COLOUR_INCREMENT })}
+        onDecrease={() => dispatch({ type: "change_red", payload: -1 * COLOUR_INCREMENT })}
         />
       <ColourPickerDetail
         colour="Green"
-        onIncrease={() => dispatch({ colourToChange: "green", amount: COLOUR_INCREMENT })}
-        onDecrease={() => dispatch({ colourToChange: "green", amount: -1 * COLOUR_INCREMENT })}
+        onIncrease={() => dispatch({ type: "change_green", payload: COLOUR_INCREMENT })}
+        onDecrease={() => dispatch({ type: "change_green", payload: -1 * COLOUR_INCREMENT })}
         />
       <ColourPickerDetail
         colour="Blue"
-        onIncrease={() => dispatch({ colourToChange: "blue", amount: COLOUR_INCREMENT })}
-        onDecrease={() => dispatch({ colourToChange: "blue", amount: -1 * COLOUR_INCREMENT })}
+        onIncrease={() => dispatch({ type: "change_blue", payload: COLOUR_INCREMENT })}
+        onDecrease={() => dispatch({ type: "change_blue", payload: -1 * COLOUR_INCREMENT })}
       />
       <View style={{
         height: 100,
