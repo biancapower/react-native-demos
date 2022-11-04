68ac7b36ce079b6106c6fbc55a590353fd9b71c3
biancapower <bianca@biancapower.com>
2022-11-05 09:48:14 +1100

    Tidy

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -6,10 +6,12 @@ const TextScreen = () => {
 
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         // TextInputs have no styles by default
         // except for the height
         style={styles.input}
+
         // handle iOS things
         autoCapitalize="none" // none, sentences, words, characters
         autoCorrect={false}  // true, false
